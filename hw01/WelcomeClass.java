a808599c5322d13208c8f7fa169b866f0976ce2c
xervitz <asamin@comcast.net>
2015-01-23 13:31:44 +0000

    Adding in HW01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+/*
+Miles Necker
+Cse002 HW 01
+
+Simple text output
+*/
+
+public class WelcomeClass { //Create class
+
+    public static void main(String[] args) { //create main method
+        
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-M--T--N--2--1--7->");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println(" v  v  v  v  v  v");
+        System.out.println("I am a professional celtic fiddle player who DJs on the side.");
+        
+    }
+
+}
