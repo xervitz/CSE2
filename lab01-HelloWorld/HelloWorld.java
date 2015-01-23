30e4581c18c4c314c4b4262ced56fe10830ba67c
xervitz <asamin@comcast.net>
2015-01-23 12:43:38 +0000

    added first lab

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+Miles Necker
+Lab 01
+Hello World
+Outputs text
+*/
+public class HelloWorld { //create class
+    
+    public static void main(String[] args) { //create main method
+        
+        System.out.println("########################"); //Output text
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("testing");
+            
+    }//close main method
+    
+}//close class
